4af13cd49e67363d0a16009a6e1ce45efc6df329
Alberto Ayala Rodriguez <alberto.ayala@newshore.es>
2022-03-05 20:29:10 -0500

    everything is done :D

--- modules/follower/router.js
+++ modules/follower/router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const FollowerController = require('./controller');
+const { canAccess } = require('../../middleware/auth/auth.middleware');
 
 const Controller = new FollowerController();
 const router = express.Router();
 
-router.post('/follow', (req, res) => { Controller.followUser(req, res) });
-router.delete('/un-follow', (req, res) => { Controller.unFollowUser(req , res) });
+router.post('/follow', [canAccess],(req, res) => { Controller.followUser(req, res) });
+router.delete('/un-follow', [canAccess],(req, res) => { Controller.unFollowUser(req , res) });
 
 module.exports = router;
